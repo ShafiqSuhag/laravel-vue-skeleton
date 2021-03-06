e86c12edf0702c64f323024ab4760c7b8c21dcb7
Shafiq Suhag <shafiqsuhag@gmail.com>
2021-03-06 20:25:46 +0600

    test by rewrite rules.

--- rewriteRules.php
+++ rewriteRules.php
@@ -6,4 +6,5 @@ return [
     'config/mypackage.php' => 'config/:lc:package.php',
     'src/Facades/MyPackage.php' => 'src/Facades/:uc:package.php',
     'src/MyPackageServiceProvider.php' => 'src/:uc:packageServiceProvider.php',
+    'resources/assets/js/MyComponent.js' => 'src/:component_name:MyComponent.js',
 ];
